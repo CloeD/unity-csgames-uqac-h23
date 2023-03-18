bfe2b95925aa4a1a7da1e14b37acfe2c1d6a4a38
pjbeltran <paul-joseph.beltran.1@ens.etsmtl.ca>
2023-03-18 15:32:56 -0400

    add reload auto et tape R

--- Assets/Game/Weapon/Weapon.cs
+++ Assets/Game/Weapon/Weapon.cs
@@ -8,13 +8,52 @@ public class Weapon : MonoBehaviour
     public GameObject _BallSpwanPoint;
     [SerializeField] 
     public GameObject _Ball;
+    [SerializeField]
+    public int _MaxAmmo;
+    [SerializeField]
+    public float _ReloadTimer = 2f;
+    [SerializeField]
+    public int _initAmmo = 10;
+
+
+    private float _StartTimeReload;
+    private bool _CanShot = true;
+    private int _CurrentAmmo;
+
+    void start()
+    {
+        _CurrentAmmo = _initAmmo;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+
+        if (_CurrentAmmo > 0 && _CanShot)
         {
-            Instantiate(_Ball, _BallSpwanPoint.transform.position, _BallSpwanPoint.transform.rotation);
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                _CanShot = false;
+            }
+            else if (Input.GetButtonDown("Fire1"))
+            {
+                Instantiate(_Ball, _BallSpwanPoint.transform.position, _BallSpwanPoint.transform.rotation);
+                _CurrentAmmo--;
+            }
+        }
+        else
+        {
+            if (_StartTimeReload >= _ReloadTimer)
+            {
+                _CanShot = true;
+                _StartTimeReload = 0;
+                _CurrentAmmo = _initAmmo;
+            }
+            else
+            {
+                _CanShot = false;
+                _StartTimeReload += Time.deltaTime;
+            }
         }
     }
 }
